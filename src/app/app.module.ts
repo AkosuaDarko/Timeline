40e5ca5a7bf1a664ff7bc2d28cc8694b666b0237
Akosua Adobea Adin-Darko <akosuaadobeaadin-darko@Akosuas-MacBook-Air.local>
2018-03-17 04:36:54 +0000

    ADDED

---
+++ src/app/app.module.ts
@@ -0,0 +1,47 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth'
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+
+const firebaseAuth = {
+    apiKey: "AIzaSyBqcpOPeRJAbbbACiStw2sjdMJWaSIbjxw",
+    authDomain: "timeline-6e270.firebaseapp.com",
+    databaseURL: "https://timeline-6e270.finrebaseio.com",
+    projectId: "timeline-6e270",
+    storageBucket: "timeline-6e270.appspot.com",
+    messagingSenderId: "268210978652"
+  };
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseAuth),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
